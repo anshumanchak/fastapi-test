5b7ab4e50374df70664ad535680c2723f29850df
Anshuman Chakravarty <anshuchak063@gmail.com>
2024-01-02 18:23:21 +0530

    Update callback_server.py

--- callback_server.py
+++ callback_server.py
@@ -7,12 +7,12 @@ app = FastAPI()
 class AtomFeed(BaseModel):
     data: str
 
-# @app.post("/callback")
-# async def receive_notification(feed_data: AtomFeed):
-#     received_data = feed_data.data
-#     print("Received Atom Feed Data:")
-#     print(received_data)
-#     return {"received_data": received_data}
+@app.post("/callback")
+async def receive_notification(feed_data: AtomFeed):
+    received_data = feed_data.data
+    print("Received Atom Feed Data:")
+    print(received_data)
+    return {"received_data": received_data}
 
 @app.get("/")
 async def print_root():
