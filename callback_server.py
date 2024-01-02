da6f42e3cbc510873121493fcc929d442a16053f
Anshuman Chakravarty <anshuchak063@gmail.com>
2024-01-02 17:49:02 +0530

    Update callback_server.py

--- callback_server.py
+++ callback_server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-# import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET
 
 app = FastAPI()
 
@@ -17,3 +17,10 @@ async def receive_notification(feed_data: AtomFeed):
 @app.get("/")
 async def print_root():
     return "Hi, you are inside disag"
+
+@app.get("/callback")
+async def receive_notification(feed_data: AtomFeed):
+    received_data = feed_data.data
+    print("Received Atom Feed Data:")
+    print(received_data)
+    return {"received_data": received_data}
